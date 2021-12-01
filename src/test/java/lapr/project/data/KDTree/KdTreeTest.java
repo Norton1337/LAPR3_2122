4441dac2671e42146a5d2104e7cb891e42e5ba65
Gonçalo Nobre <8190593@estg.ipp.pt>
2021-12-01 23:37:06 +0000

    <LAP22G128-31> Fixing Jenkins #3

--- src/test/java/lapr/project/data/KDTree/KdTreeTest.java
+++ src/test/java/lapr/project/data/KDTree/KdTreeTest.java
@@ -4,29 +4,25 @@ import org.junit.jupiter.api.Test;
 
 import lapr.project.data.kdtree.KdTree;
 import lapr.project.data.kdtree.Node;
-import oracle.ucp.common.waitfreepool.kdtree.KDTree;
 
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class KdTreeTest {
 
     KdTree kdTree = new KdTree();
     Node node = new Node("elem", 62.87, -21.5);
 
-
-
     @Test
-    void isEmptyTest(){
+    void isEmptyTest() {
         assertEquals(0, kdTree.getAllElements().size());
     }
-    
+
     @Test
-    void heightTest(){
+    void heightTest() {
         kdTree.insert("elem4", 10.6, 50.8);
         kdTree.insert("elem5", 11.6, -43.2);
         kdTree.insert("elem6", 12.6, 24.9);
@@ -34,9 +30,9 @@ class KdTreeTest {
     }
 
     @Test
-    <T> void buildBalancedTreeTest(){
+    <T> void buildBalancedTreeTest() {
         List<Node<T>> nodeList = new ArrayList<>();
-        
+
         nodeList.add(new Node("elem", 54.4, 87.1));
         nodeList.add(new Node("elem2", 63.7, -41.2));
         nodeList.add(new Node("elem3", -36.8, 27.9));
@@ -47,19 +43,20 @@ class KdTreeTest {
         nodeList.add(new Node("elem8", 14.6, 26.9));
         kdTree = new KdTree(nodeList);
         assertEquals(3, kdTree.height());
-        
+
     }
 
     @Test
-    void insertDeleteSizeTest(){
+    void insertDeleteSizeTest() {
         kdTree.insert("elem", -85.14, -24.8);
         kdTree.insert("elem2", 78.2, 21.4);
         assertEquals(2, kdTree.size());
         kdTree.delete("elem");
         assertEquals(1, kdTree.size());
     }
+
     @Test
-    void getAllElementsTest(){
+    void getAllElementsTest() {
 
         kdTree.insert("elem", 54.4, 87.1);
         kdTree.insert("elem2", 63.7, -41.2);
@@ -70,7 +67,7 @@ class KdTreeTest {
     }
 
     @Test
-    void findTest(){
+    void findTest() {
 
         Node node = kdTree.insert("elem", -58.14, 70.2);
 
