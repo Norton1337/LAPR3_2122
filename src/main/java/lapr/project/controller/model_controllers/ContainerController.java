5efb318f13056f0029c5245e08952f75069541e9
goncalor21 <1190610@isep.ipp.pt>
2022-01-21 00:21:44 +0000

    <LAP22G128-198> Implemented function whereIsContainer from DB.

--- src/main/java/lapr/project/controller/model_controllers/ContainerController.java
+++ src/main/java/lapr/project/controller/model_controllers/ContainerController.java
@@ -1,24 +1,68 @@
 package lapr.project.controller.model_controllers;
 
+import jdk.vm.ci.meta.Local;
+import lapr.project.model.cargoManifest.CargoManifest;
 import lapr.project.model.containers.Container;
 import lapr.project.model.containers.idb.IContainerDB;
-import lapr.project.model.ships.Generator;
-import lapr.project.model.ships.Ship;
+import lapr.project.model.locals.Locals;
+import lapr.project.model.operation.Operation;
+import lapr.project.model.vehicle.Vehicles;
 
-import java.util.ArrayList;
 import java.util.List;
+
+import static lapr.project.utils.Utils.toDate;
+import static lapr.project.utils.Utils.toInt;
 
 public class ContainerController {
 
     private final IContainerDB containerDB;
+    private final CargoManifestController cargoManifestController;
+    private final OperationController operationController;
+    private final VehiclesController vehiclesController;
+    private final LocalsController localsController;
 
-    public ContainerController(IContainerDB containerDB) {
+    public ContainerController(IContainerDB containerDB, CargoManifestController cargoManifestController,
+                               OperationController operationController, VehiclesController vehiclesController, LocalsController localsController) {
         this.containerDB = containerDB;
+        this.cargoManifestController = cargoManifestController;
+        this.operationController = operationController;
+        this.vehiclesController = vehiclesController;
+        this.localsController = localsController;
     }
 
     public boolean addContainer(Container newContainer) {
         containerDB.addContainer(newContainer);
         return true;
+    }
+
+    public String whereIsContainer(String containerNumber, String userName, String leas){
+        String whereIsContainer;
+        CargoManifest cargoManifest = null;
+        Container container = null;
+        for(Container elem : containerDB.getAllContainers()){
+            if(elem.getContainerNumber() == toInt(containerNumber)){
+                container = elem;
+            }
+        }
+        for(Operation elem : operationController.getAllOperations()){
+            if(elem.getContainerId().equals(container.getId())){
+                if(cargoManifest == null) {
+                    cargoManifest = cargoManifestController.findCargoById(elem.getCargoManifestId());
+                }else if(toDate(cargoManifest.getDate()).compareTo
+                        (toDate(cargoManifestController.findCargoById(elem.getCargoManifestId()).getDate())) < 0){
+                    cargoManifest = cargoManifestController.findCargoById(elem.getCargoManifestId());
+                }
+            }
+        }
+
+        if(cargoManifest.getOperationType().equals("Load")){
+            Vehicles vehicles = vehiclesController.getVehicle(cargoManifest.getVehicleId());
+            return "Container is on vehicle: " + vehicles.getVehicle_recon();
+        }else if(cargoManifest.getOperationType().equals("Unload")){
+            Locals local = localsController.getLocalWithPortId(cargoManifest.getCurrentLocalId());
+            return "Container is on port: " + String.valueOf(local.getLocalCode());
+        }
+        return "Container not found.";
     }
 
     public List<Container> getAllContainers() {
--- src/main/java/lapr/project/ui/Main.java
+++ src/main/java/lapr/project/ui/Main.java
@@ -42,8 +42,8 @@ class Main {
         GeneratorController generatorController = new GeneratorController(shipDBMock, generatorDBMock);
         LocalsController localsController = new LocalsController(countryDBMock, localsDBMock);
         CargoManifestController cargoManifestController = new CargoManifestController(vehiclesDBMock, cargoManifestDBMock, operationDBMock);
-        ContainerController containerController = new ContainerController(containerDBMock);
         OperationController operationController = new OperationController(operationDBMock,containerDBMock, localsController, cargoManifestController, shipController, vehiclesController);
+        ContainerController containerController = new ContainerController(containerDBMock, cargoManifestController, operationController, vehiclesController, localsController);
         TruckController truckController = new TruckController(trucksDBMock);
         UserController userController = new UserController(usersDBMock);
         ClientController clientController = new ClientController(clientDBMock, usersDBMock);
