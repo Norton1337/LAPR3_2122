831e89cd08019ccba1e8a5a79de6e6dfa287e6c1
Tiago Marante <tiagofmarante@gmail.com>
2022-01-22 04:04:54 +0000

    jenkins poml fix

--- src/test/java/lapr/project/cargoShipUserStoriesTest/ShapesTest.java
+++ src/test/java/lapr/project/cargoShipUserStoriesTest/ShapesTest.java
@@ -1,9 +1,7 @@
 package lapr.project.cargoShipUserStoriesTest;
 
-import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
