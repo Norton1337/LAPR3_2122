8201a4210025323bfa5979e45c096e4a18776b83
Tiago Marante <tiagofmarante@gmail.com>
2022-01-23 17:04:04 +0000

    <LAP22G128-194> more test for the project

---
+++ src/test/java/lapr/project/controller/model_controllers/ContainerControllerTest.java
@@ -0,0 +1,28 @@
+package lapr.project.controller.model_controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContainerControllerTest {
+
+    @Test
+    void addContainer() {
+    }
+
+    @Test
+    void whereIsContainer() {
+    }
+
+    @Test
+    void containerRoute() {
+    }
+
+    @Test
+    void getAllContainers() {
+    }
+
+    @Test
+    void findContainerByNumber() {
+    }
+}
